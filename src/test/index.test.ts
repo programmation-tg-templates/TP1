981d7df2ebd8497b29daa8fc7be5c2e1de742820
François ROLAND <francois.roland@heh.be>
2025-09-28 16:42:47 +0200

    Import Gianni's tests.

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,9 +1,17 @@
 // Tests unitaires écrits par l'enseignant. Ne modifiez pas ce fichier.
 
-import {...} from '../index';
+import {estBissextile} from '../index';
 
-describe('...', () => {
-    test('...', () => {
-        expect(false).toBe(true);
+describe('Vérifier si une année donnée est bissextile ou non', () => {
+    test('2024 est bissextile ', () => {
+        expect(estBissextile(2024)).toEqual(true);
+    });
+
+    test('1900 n\'est pas bissextile', () => {
+        expect(estBissextile(1900)).toEqual(false);
+    });
+
+    test('2000 est bissextile', () => {
+        expect(estBissextile(2000)).toEqual(true);
     });
 });
